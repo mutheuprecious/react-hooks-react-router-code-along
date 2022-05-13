d161593c05393bd7f95ab5012e6c86b611fc90d6
Precious <preciousmaregwa1997@gmail.com>
2022-05-13 22:29:17 +0300

    complete assigment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar() {
+  return (
+    <div>
+      <NavLink
+        to="/"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        
+        /* add styling to Navlink */
+        style={linkStyles}
+        /* add prop for activeStyle */
+        activestyle={{
+          backgroundColor: "red",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        
+        style={linkStyles}
+        activestyle={{
+          background: "red",
+        }}
+      >
+        About
+      </NavLink>
+    </div>
+  );
+}
 
 function Home() {
   return (
@@ -8,9 +49,32 @@ function Home() {
     </div>
   );
 }
+function About() {
+  return (
+    <div>
+      <h1>About!</h1>
+    </div>
+  );
+}
+function NotFound() {
+  return (
+    <div>
+      <h1>Page Not Found!</h1>
+    </div>
+  );
+}
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
